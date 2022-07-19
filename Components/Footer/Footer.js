933f54164171a7cd1aab5fb8305c0b76bab27952
IloveGreeny <greenxd23@gmail.com>
2022-07-19 17:55:12 +0400

    added footer

---
+++ Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import {Text, View, ScrollView} from 'react-native';
+
+export default function Footer() {
+    return(
+        <View>
+            <ScrollView>
+                <Text>Footer</Text>
+            </ScrollView>
+        </View>
+    );
+}
