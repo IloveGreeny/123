b28166f555e65fa86f33273eab8a4e4ee18ebdcd
IloveGreeny <greenxd23@gmail.com>
2022-07-20 13:57:52 +0400

    foooter

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -1,11 +1,16 @@
-import {Text, View, ScrollView} from 'react-native';
+import * as React from 'react';
+import { Text, View, ScrollView } from 'react-native';
+import Constants from 'expo-constants';
+
+import { Card } from 'react-native-paper';
 
 export default function Footer() {
-    return(
-        <View>
-            <ScrollView>
-                <Text>Footer</Text>
-            </ScrollView>
-        </View>
-    );
-}
+  return (
+    <View style={{ flex: 1 , backgroundColor : "green"}}>
+      <ScrollView>main</ScrollView>
+      <View>
+        <Text>footer</Text>
+      </View>
+    </View>
+  );
+};
